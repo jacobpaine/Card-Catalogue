4f0fef72dc514c58542d3c801fb40a3d25ea0d4a
Jacob Paine <painejacob@gmail.com>
2016-10-07 14:22:51 -0500

    Package alterations

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,7 +31,7 @@ var jsFilesToInject = [
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
-  'bower_components/angular/angular.js',
+  '/bower_components/angular/angular.js',
   '/bower_components/jquery/dist/jquery.js',
   '/bower_components/angular-route/angular-route.js',
   '/bower_components/tether/dist/js/tether.min.js',
